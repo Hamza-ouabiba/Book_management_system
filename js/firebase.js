b6fd27a97db5a35afa546f73bce2d4bbbf260a7f
hamza <hamza.ouabiba@gmail.com>
2021-09-03 11:32:03 +0100

    master

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+        // Import the functions you need from the SDKs you need
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+          apiKey: "AIzaSyBjnjOLNcpdCO1JN7lmEUp_fxcaXJReVTk",
+          authDomain: "book-management-system-3b821.firebaseapp.com",
+          projectId: "book-management-system-3b821",
+          storageBucket: "book-management-system-3b821.appspot.com",
+          messagingSenderId: "536599902616",
+          appId: "1:536599902616:web:9f0b8b24aae8eb1bb113ed"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore()
+        db.collection("Book").get()
+        .then((res) => {
+             res.forEach(book => console.log(book.data()));
+        })
+        //add it to the user interface : 
