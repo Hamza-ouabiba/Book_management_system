21467051a03b4eae4b356ff6da567ea37f73e01c
hamza <hamza.ouabiba@gmail.com>
2021-09-07 00:27:53 +0100

    master

--- js/show_list.js
+++ js/show_list.js
@@ -1,4 +1,5 @@
 let tbody = document.querySelector('tbody');
+let button_ = document.querySelector('.but');
 //getting all the data form the database :
 db.collection("Book").get()
 .then(function(books)
@@ -16,4 +17,17 @@ db.collection("Book").get()
         `
         tbody.innerHTML += html;
     })
+})
+//
+button_.addEventListener('click',function(event)
+{
+     event.preventDefault();
+     firebase.auth().signOut()
+     .then(res => 
+        {
+             alert('log out succesfull');
+             console.log("log out succesfull : ",res);
+             document.location.href = "/Authentication/auth.html";
+        })
+        .catch(error => console.log(error));
 })
