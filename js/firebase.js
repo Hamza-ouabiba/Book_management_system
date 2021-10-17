8a83485879abc5167343677b9bcc26e11e5c38f0
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2021-10-17 20:46:43 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = 
        {
           apiKey: "AIzaSyBjnjOLNcpdCO1JN7lmEUp_fxcaXJReVTk",
